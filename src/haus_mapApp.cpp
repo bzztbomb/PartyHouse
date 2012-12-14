4b18fb7dcd31d702efdad96697d1f5cc07db5b2d
Brian Richardson <skinny@knowhere.net>
2012-12-13 19:02:25 -0800

    Able to select a point

--- src/haus_mapApp.cpp
+++ src/haus_mapApp.cpp
@@ -9,11 +9,14 @@ using namespace std;
 
 struct QuadSurface
 {
-    Rectf coords;
+    // We assume 4 coords
+    PolyLine<Vec2f> coords;
 };
 
 class haus_mapApp : public AppBasic {
 public:
+    haus_mapApp();
+    
     void setup();
     void mouseDown( MouseEvent event );	
     void update();
@@ -23,10 +26,20 @@ private:
     Rectf mInputRect;
     gl::Texture mInput;
     vector<QuadSurface> mSurfaces;
+    Vec2f* mActivePoint;
     
-    Rectf surfToEditor(const Rectf& input);
+    PolyLine<Vec2f> surfToEditor(const PolyLine<Vec2f>& input);
+    Vec2f surfToEditor(const Vec2f& input);
     Rectf editorToSurf(const Rectf& input);
 };
+
+const float HANDLE_SIZE = 4.0f;
+
+haus_mapApp::haus_mapApp() :
+    mActivePoint(NULL)
+{
+    
+}
 
 void haus_mapApp::prepareSettings(Settings* settings)
 {
@@ -37,27 +50,56 @@ void haus_mapApp::setup()
 {
     mInput = gl::Texture(loadImage(loadResource("test.jpg")));
     QuadSurface q;
-    q.coords = Rectf(0.0f, 0.0f, 1.0f, 1.0f);
+    q.coords.push_back(Vec2f(0.0f, 0.0f));
+    q.coords.push_back(Vec2f(1.0f, 0.0f));
+    q.coords.push_back(Vec2f(1.0f, 1.0f));
+    q.coords.push_back(Vec2f(0.0f, 1.0f));
+    q.coords.setClosed();
     mSurfaces.push_back(q);
     const Rectf editor_rect = getWindowBounds();
     mInputRect = Rectf(editor_rect.x1, editor_rect.y2 / 2, editor_rect.x2 / 2, editor_rect.y2);
+//    setFullScreen(true);
+    DisplayRef dr = Display::getMainDisplay();
+    printf("display: w: %d, h: %d", dr->getWidth(), dr->getHeight());
+    printf("window: w: %d, h: %d", getWindowWidth(), getWindowHeight());
 }
 
 void haus_mapApp::mouseDown( MouseEvent event )
 {
+    const Vec2f ev_pos(event.getX(), event.getY());
+    for (auto surf = mSurfaces.begin(); surf != mSurfaces.end(); surf++)
+    {
+        for (auto i = surf->coords.begin(); i != surf->coords.end(); i++)
+        {
+            Vec2f v = surfToEditor(*i) - ev_pos;
+            if (v.lengthSquared() < HANDLE_SIZE*HANDLE_SIZE)
+            {
+                mActivePoint = &*i;
+            }
+        }
+    }
 }
 
 void haus_mapApp::update()
 {
 }
 
-Rectf haus_mapApp::surfToEditor(const Rectf& input)
+Vec2f haus_mapApp::surfToEditor(const Vec2f& input)
 {
-    Rectf result;
-    result.x1 = input.x1 * (mInputRect.x2 - mInputRect.x1) + mInputRect.x1;
-    result.x2 = input.x2 * (mInputRect.x2 - mInputRect.x1) + mInputRect.x1;
-    result.y1 = input.y1 * (mInputRect.y2 - mInputRect.y1) + mInputRect.y1;
-    result.y2 = input.y2 * (mInputRect.y2 - mInputRect.y1) + mInputRect.y1;
+    Vec2f v;
+    v.x = input.x * (mInputRect.x2 - mInputRect.x1) + mInputRect.x1;
+    v.y = input.y * (mInputRect.y2 - mInputRect.y1) + mInputRect.y1;
+    return v;
+}
+
+PolyLine<Vec2f> haus_mapApp::surfToEditor(const PolyLine<Vec2f>& input)
+{
+    PolyLine<Vec2f> result;
+    for (auto i = input.begin(); i != input.end(); i++)
+    {
+        result.push_back(surfToEditor(*i));
+    }
+    result.setClosed();
     return result;
 }
 
@@ -82,16 +124,21 @@ void haus_mapApp::draw()
     gl::color(Color::white());
     gl::draw(mInput, input_rect);
 
+    // Surfaces
     for (auto surf = mSurfaces.begin(); surf != mSurfaces.end(); surf++)
     {
-        const Rectf quad0 = surfToEditor(surf->coords);
+        const PolyLine<Vec2f> quad0 = surfToEditor(surf->coords);
         gl::color(1.0f, 1.0f, 1.0f);
-        gl::drawStrokedRect(quad0);
-        gl::color(1.0f, 1.0f, 0.0f);
-        gl::drawSolidCircle(Vec2f(quad0.x1, quad0.y1), 4.0f);
-        gl::drawSolidCircle(Vec2f(quad0.x1, quad0.y2), 4.0f);
-        gl::drawSolidCircle(Vec2f(quad0.x2, quad0.y2), 4.0f);
-        gl::drawSolidCircle(Vec2f(quad0.x2, quad0.y1), 4.0f);
+        gl::draw(quad0);
+
+        for (auto i = surf->coords.begin(); i != surf->coords.end(); i++)
+        {
+            if (&*i != mActivePoint)
+                gl::color(1.0f, 1.0f, 0.0f);
+            else
+                gl::color(1.0f, 0.0f, 0.0f);
+            gl::drawSolidCircle(surfToEditor(*i), HANDLE_SIZE);
+        }
     }
 }
 
